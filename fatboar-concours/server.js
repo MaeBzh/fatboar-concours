ce92f0b5b740f48a92dcf4c23356a32882c16b71
MaeBzh <maelenn.picaud@gmail.com>
2022-06-05 20:09:57 +0200

    redirect to www and https

--- fatboar-concours/server.js
+++ fatboar-concours/server.js
@@ -2,8 +2,9 @@ var express = require("express");
 var history = require("connect-history-api-fallback");
 var https = require("https");
 var fs = require("fs");
-var compression = require("compression");
-const { resolve } = require("path");
+var { redirectToHTTPS } = require("express-http-to-https");
+var { redirectNoWWWToWWW } = require("express-www-redirect");
+var { resolve } = require("path");
 
 var httpsOptions = {
   cert: fs.readFileSync(resolve(__dirname, "../server.cert"), "utf8"),
@@ -11,6 +12,11 @@ var httpsOptions = {
 };
 
 var app = express();
+
+// redirect http to https
+app.use(redirectToHTTPS());
+// redirect no-www to www
+app.use(redirectNoWWWToWWW());
 
 // Middleware for serving '/dist' directory
 const staticFileMiddleware = express.static("dist");
@@ -28,8 +34,6 @@ app.use(
 
 // 2nd call for redirected requests
 app.use(staticFileMiddleware);
-
-app.use(compression());
 
 app.all("*", (_req, res) => {
   try {
