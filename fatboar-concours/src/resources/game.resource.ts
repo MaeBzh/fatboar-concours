1aad079d79fa869920deba190c19e46cdfb76a32
MaeBzh <maelenn.picaud@gmail.com>
2022-06-05 10:59:23 +0200

    update charts views

--- fatboar-concours/src/resources/game.resource.ts
+++ fatboar-concours/src/resources/game.resource.ts
@@ -17,13 +17,19 @@ export class GameResource extends ApiResource<Game> {
     return data;
   }
 
-  /**
-   * File must be send with multipart/formdata
-   *
-   * @param item
-   * @returns
-   */
-  async create(item: Game & { gameGifts: Partial<GameGift>[] }): Promise<Game> {
+  async getGameStats(item: Game): Promise<{
+    used: number;
+    unused: number;
+    unassigned: number;
+  }> {
+    const { data } = await this.http.get(`${this.basePath}/${item.id}/stats`);
+    return data;
+  }
+
+  protected formatMultipartFormData(
+    item: Game & { gameGifts: Partial<GameGift>[] }
+  ) {
+    item.gameGifts = item.gameGifts.filter((gg) => gg.winPercentage > 0);
     const form = new FormData();
     Object.keys(item).forEach((key) => {
       const value =
@@ -33,7 +39,21 @@ export class GameResource extends ApiResource<Game> {
           : item[key];
       form.append(key, value);
     });
-    const { data } = await this.http.post(this.basePath, form);
+
+    return form;
+  }
+
+  /**
+   * File must be send with multipart/formdata
+   *
+   * @param item
+   * @returns
+   */
+  async create(item: Game & { gameGifts: Partial<GameGift>[] }): Promise<Game> {
+    const { data } = await this.http.post(
+      this.basePath,
+      this.formatMultipartFormData(item)
+    );
     return data;
   }
 
@@ -44,16 +64,9 @@ export class GameResource extends ApiResource<Game> {
    * @returns
    */
   async update(item: Game & { gameGifts: Partial<GameGift>[] }): Promise<void> {
-    const form = new FormData();
-    Object.keys(item).forEach((key) => {
-      const value =
-        Array.isArray(item[key]) ||
-        Object.prototype.toString.call(item[key]) === "[object Object]"
-          ? JSON.stringify(item[key])
-          : item[key];
-
-      form.append(key, value);
-    });
-    await this.http.put(`${this.basePath}/${item.id}`, form);
+    await this.http.put(
+      `${this.basePath}/${item.id}`,
+      this.formatMultipartFormData(item)
+    );
   }
 }
