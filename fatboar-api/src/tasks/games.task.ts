195a1161967d8c45d5c2094c62f1a35aa62ec016
MaeBzh <maelenn.picaud@gmail.com>
2022-06-26 14:48:36 +0000

    add a scheduler to activate or desactivate games

---
+++ fatboar-api/src/tasks/games.task.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { Cron, CronExpression } from "@nestjs/schedule";
+import { isToday, isYesterday } from "date-fns";
+import { GamesService } from "../games/games.service";
+
+@Injectable()
+export class GameTask {
+  constructor(private gameService: GamesService) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_5PM)
+  handleCron() {
+    this.endGame().then(() => this.startGame());
+  }
+
+  protected async endGame(): Promise<void> {
+    const currentGame = await this.gameService.findCurrentGame();
+    if (currentGame) {
+      if (isYesterday(new Date(currentGame.endsOn))) {
+        this.gameService.update(currentGame.id, {
+          ...currentGame,
+          activated: false,
+        });
+      }
+    }
+  }
+
+  protected async startGame(): Promise<void> {
+    const currentGame = await this.gameService.findCurrentGame();
+    if (!currentGame) {
+      const games = await this.gameService.findAll();
+      games.forEach((game) => {
+        if (isToday(new Date(game.startsOn))) {
+          this.gameService.update(currentGame.id, { ...game, activated: true });
+        }
+      });
+    }
+  }
+}
